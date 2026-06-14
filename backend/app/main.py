5de3213874cb0b2377a94c83defd319faf55577b
louise martins <louisemartins0303@gmail.com>
2026-06-14 14:35:06 -0300

    Commit coordenador

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,16 +4,12 @@ from app.routes import professor_routes
 from app.routes import empresa_routes
 from app.routes import coordenador_routes
 
-
 app = FastAPI()
 
 app.include_router(aluno_routes.router)
 app.include_router(professor_routes.router)
-<<<<<<< HEAD
 app.include_router(empresa_routes.router)
-=======
 app.include_router(coordenador_routes.router)
->>>>>>> 33e6bed (Backend com rotas de coordenador testadas no Swagger)
 
 @app.get("/")
 def home():
